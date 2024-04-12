7431dbff59536f27bb60c27a45db3e521468d55e
Mr. Kuldeep <128669361+MrKuldeep01@users.noreply.github.com>
2024-04-12 15:43:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,9 +8,7 @@ function setCard(obj) {
       <img src="${obj["avatar_url"]}" class="img" alt="demoimg" />
     </div>
     <span class="employname"> <i class="ri-user-2-fill"></i> ${obj["name"]}</span>
-    <a target="_blank" href="${
-      obj["html_url"]
-    }" class="employrole button" title="GitHub Profile"><i class="ri-github-fill"></i> ${obj["login"]}</a>
+    <a target="_blank" href="${obj["html_url"]}" class="employrole button" title="GitHub Profile"><i class="ri-github-fill"></i> ${obj["login"]}</a>
 
     <span class="location"><i class="ri-map-pin-user-line"></i> ${obj["location"]}</span>
   </div>
@@ -20,12 +18,17 @@ function setCard(obj) {
       
     </p>
     <div class="leftrightbtn">
-      <span class="pointer button followers" title="Followers"> <i class="ri-user-received-fill"></i> Followers ${
-    obj["followers"]
-  }</span>
-      <span class="pointer button following" title="Following"> <i class="ri-user-shared-fill"></i> Following ${
-    obj["following"]
-  }</span>
+      <span class="pointer button followers" title="Followers" > <i class="ri-user-received-fill"></i> Followers ${obj["followers"]}</span>
+      <a class="button" title="following page" target="_blank" href="https://github.com/${obj["login"]}?tab=following">
+       <i class="ri-user-shared-fill"></i> Following ${obj["following"]}
+       </a>
+
+
+      <a class="button" title="Repositories page" target="_blank" href="https://github.com/${obj["login"]}?tab=repositories">
+       <i class="ri-file-fill"></i> Repositories
+       </a>
+
+
     </div>
   </div>
 </div>
@@ -35,13 +38,13 @@ function setCard(obj) {
 
 function getUserFromGithub() {
   const userName = document.querySelector(".userName").value;
-  if(!userName){
+  if (!userName) {
     let message = "enter userName properly";
     console.log(message);
-    alert(message)
-    return
+    alert(message);
+    return;
   }
-  let APIUrl = "https://api.github.com/users/"+userName;
+  let APIUrl = "https://api.github.com/users/" + userName;
 
   const xhr = new XMLHttpRequest();
   xhr.open("GET", APIUrl);
@@ -49,7 +52,12 @@ function getUserFromGithub() {
     if (xhr.readyState === 4) {
       try {
         let data = JSON.parse(this.responseText);
+        console.log(data);
         setCard(data);
+        document.querySelector(".followers").addEventListener("click", (e) => {
+          showFollowers(data["followers_url"]);
+        });
+        // showFollowing(data["following_url"]);
       } catch {
         alert("connection error");
         return;
@@ -58,5 +66,60 @@ function getUserFromGithub() {
   };
   xhr.send();
 }
+
+// function showFollowers(arr) {
+//   arr.map((obj, index) => {
+//     let follower = `
+//   <div class="followerCard">
+//   <div class="employimage">
+//     <img src="${obj["avatar_url"]}" class="img" alt="demoimg" />
+//   </div>
+//   <span class="employname"> <i class="ri-user-2-fill"></i> ${obj["name"]}</span>
+//   <a target="_blank" href="${obj["html_url"]}" class="employrole button" title="GitHub Profile"><i class="ri-github-fill"></i> ${obj["login"]}</a>
+
+//   <span class="location"><i class="ri-map-pin-user-line"></i> ${obj["location"]}</span>
+// </div>
+//   `;
+//   });
+// }
+// function showFollowing(url) {
+function showFollowers(url) {
+  let arr = [];
+  fetch(url)
+    .then((res) => res.json())
+    .then((res) => {
+      arr = res;
+      console.log(res);
+
+      let followersContainer = document.querySelector(".followersContainer");
+      followersContainer.innerHTML = `
+      
+      <button class="backbtn button">Go Back</button>
+      `;
+      arr.map((obj, index) => {
+        let follower = `
+  <div class="followerCard">
+  <div class="employimage">
+    <img src="${obj["avatar_url"]}" class="img" alt="demoimg" />
+  </div>
+  <a target="_blank" href="${obj["html_url"]}" class="employrole button" title="GitHub Profile"><i class="ri-github-fill"></i> ${obj["login"]}</a>
+</div>
+  `;
+        followersContainer.style.left = 0;
+        followersContainer.innerHTML += follower;
+      });
+
+      document.querySelector(".backbtn").addEventListener("click", (e) => {
+        console.log("clicked");
+        followersContainer.style.left = "-100%";
+        //  let follower = document.querySelector(".follower").remove();
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
 // console.log("script running");
 //  git_repo: " https://github.com/MrKuldeep01/github-user-searcher.git"
+
